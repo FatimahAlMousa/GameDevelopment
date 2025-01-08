9114a80e5ad8b7a219f8042f666f170e90bc203e
Fatimah Al Mousa <2220005106@iau.edu.sa>
2025-01-08 19:12:29 +0300

    add jump but need to edit not work

--- Assets/SquareMovement.cs
+++ Assets/SquareMovement.cs
@@ -1,53 +1,50 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class SquareMovement : MonoBehaviour
-{ public Rigidbody2D rg;
-  public float jumpForce = 3f; 
-  
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        rg = GetComponent<Rigidbody2D>();
-
-        // Check if Rigidbody2D is missing and log an error if needed
-        if (rg == null)
-        {
-            Debug.LogError("Rigidbody2D component is missing from this object!");
-        }
-
-    }
+{
+    public Rigidbody2D rg; // Rigidbody2D for physics-based movement
+    public float jumpForce = 300f; // Adjust to control jump height
+    public Transform groundCheck; // Empty GameObject to check ground
+    public LayerMask groundLayer; // LayerMask to specify the ground layer
+    [SerializeField] int jump;
+    bool isGrounded;
 
     // Update is called once per frame
     void Update()
     {
         // Move Right
-        if (Input.GetKey(KeyCode.D) || Input.GetAxis("Horizontal") > 0)
+        if (Input.GetKey(KeyCode.D))
         {
             transform.position += Vector3.right * 0.01f;
         }
         // Move Left
-        else if (Input.GetKey(KeyCode.A) || Input.GetAxis("Horizontal") < 0)
+        else if (Input.GetKey(KeyCode.A))
         {
             transform.position += Vector3.left * 0.01f;
         }
+
         // Jump
-        else if (Input.GetKeyDown(KeyCode.W) || Input.GetAxis("Vertical") > 0)
+        isGrounded = Physics2D.OverlapCapsule(groundCheck.position,new Vector2(1.8f,0.3f),CapsuleDirection2D.Horizontal,0,groundLayer);
+        if (Input.GetButtonDown("Jump") && isGrounded)
         {
-            if (IsGrounded()) // Only jump if grounded
-            {
-                rg.velocity = new Vector2(rg.velocity.x, jumpForce);
-            }
+            rg.velocity = new Vector2(rg.velocity.x, jump);
         }
     }
 
     // Check if the object is on the ground
     private bool IsGrounded()
     {
-        // Simple ground check using velocity
-        return Mathf.Abs(rg.velocity.y) < 0.01f;
+        // Check if the groundCheck object is touching the ground layer
+        return Physics2D.OverlapCircle(groundCheck.position, 0.1f, groundLayer);
+    }
+
+    private void OnDrawGizmos()
+    {
+        // Visualize the ground check in the Scene view
+        if (groundCheck != null)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(groundCheck.position, 0.1f);
+        }
     }
 }
